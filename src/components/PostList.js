ec6a91615f891a201f61fc62d74ae3b38f36cc5d
Ali Mehdi <salimehdi144@gmail.com>
2019-05-27 00:39:47 +0500

    Create PostList.js

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostList extends React.Component{
+    render(){
+        return <div>Post List</div>;
+    }
+}
+
+export default PostList;
