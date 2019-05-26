1a9689fddf49a4075e46a6fa3fe9159aec7e8186
Ali Mehdi <salimehdi144@gmail.com>
2019-05-27 01:45:01 +0500

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostList extends React.Component{
+    
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    
     render(){
         return <div>Post List</div>;
     }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts }) (PostList);
