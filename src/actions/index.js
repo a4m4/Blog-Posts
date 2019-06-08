cae53098fd3b58bf915ca4cb4df89dfc1dfde149
Ali Mehdi <salimehdi144@gmail.com>
2019-06-08 17:43:01 +0500

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,14 @@ export const fetchPosts = () => {
 
     };
 };
+
+
+export const fetchUser = (id) => {
+
+    return async (dispatch) => {
+
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch({   type: 'FETCH_USER', payload: response.data })
+
+    };
+};
